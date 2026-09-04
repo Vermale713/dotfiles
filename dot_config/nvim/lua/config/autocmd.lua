697895dc4c00ae68c509a888f8d6157773340b75
Vermale713 <vermale713@gmail.com>
2026-09-05 05:54:45 +0800

    zig commentstring

--- dot_config/nvim/lua/config/autocmd.lua
+++ dot_config/nvim/lua/config/autocmd.lua
@@ -5,3 +5,10 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.treesitter.start(args.buf, "luau")
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "zig",
+	callback = function(args)
+		vim.bo[args.buf].commentstring = "// %s"
+	end,
+})
